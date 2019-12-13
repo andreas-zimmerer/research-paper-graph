a3f42e74398381105218bd6db83505338e14412b
Fiona Guerin <fiona.guerin@tum.de>
2019-12-13 14:40:22 +0100

    Presentation: I have deleted the template.

--- doc/presentations/template/R/ggplot_example_animation_1.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-ggplot(diamonds) +
- aes(x=carat) +
- aes(y=price) +
- facet_wrap(~cut) +
- geom_line() +
- ggsave("animation_1.pdf", width=4, height=3)
--- doc/presentations/template/R/ggplot_example_animation_3.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-ggplot(subset(diamonds, color %in% c("D"))) +
- aes(x=carat) +
- aes(y=price) +
- aes(col=clarity) +
- facet_wrap(~cut) +
- geom_line() +
- ggsave("animation_3.pdf", width=4, height=3)
