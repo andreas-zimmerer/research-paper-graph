6886cd2d9585e53e4a9cbe8d2100a2ea0174b460
Fiona Guerin <fiona.guerin@tum.de>
2019-12-14 20:43:19 +0100

    Basic Backend: I have improved the code quality.

--- backend/app/main/model/paper.py
+++ backend/app/main/model/paper.py
@@ -5,7 +5,7 @@ class Paper(db.Model): # pylint: disable=too-few-public-methods
     """ Paper Model for storing paper related details """
     __tablename__ = "paper"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(255), nullable=False)
-    year = (db.Integer)
-    abstract = db.Column(db.String(255), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) # pylint: disable=no-member
+    title = db.Column(db.String(255), nullable=False) # pylint: disable=no-member
+    year = (db.Integer) # pylint: disable=no-member
+    abstract = db.Column(db.String(255), nullable=False) # pylint: disable=no-member
