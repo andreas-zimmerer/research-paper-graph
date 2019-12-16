49c49487ccc856546860eea50b4ce89890d3e0e8
Fiona Guerin <fiona.guerin@tum.de>
2019-12-16 21:31:17 +0100

    Basic Backend: I have implemented displaying the information of toNodes.

--- backend/app/main/service/relative_service.py
+++ backend/app/main/service/relative_service.py
@@ -5,35 +5,45 @@ from collections import defaultdict
 
 def get_all_relatives(title):
     """GET paper relatives by paper title"""
-    query = "with recursive family(from_paper, title, abstract, year, to_paper) as (" \
-            "select p.*, r.to_paper " \
-            "from paper p, reference r " \
-            "where p.id == r.from_paper and p.title == '" + title + "' " \
+    query = "with recursive family(from_paper, from_title, from_abstract, from_year, to_paper, to_title, to_abstract, to_year) as (" \
+            "select pf.*, pt.* " \
+            "from paper pf, reference r, paper pt " \
+            "where pf.id == r.from_paper and pf.title == '" + title + "' and pt.id == r.to_paper " \
             "" \
             "UNION ALL " \
             "" \
-            "select f.to_paper as from_paper, p.title, p.abstract, p.year, r.to_paper " \
-            "from family f, reference r, paper p " \
-            "where f.to_paper == r.from_paper and p.id == f.to_paper) " \
+            "select f.to_paper as from_paper, f.to_title as from_title, f.to_abstract as from_abstract, f.to_year as from_year, pt.id as to_paper, pt.title as to_title, pt.abstract as to_abstract, pt.year as to_year " \
+            "from family f, reference r, paper pt " \
+            "where f.to_paper == r.from_paper and pt.id == r.to_paper) " \
             "" \
             "select * " \
             "from family "
 
-    result = db.engine.execute(query)
+    resultNode = db.engine.execute(query)
+    resultLink = db.engine.execute(query)
 
     dictionary = defaultdict(list)
-    for row in result:
-        id = row['from_paper']
-        if id in dictionary:
-            dictionary[id]['dependents'].append(row['to_paper'])
-        else:
-            dictionary[id] = {
-                "id": id,
-                "title": row['title'],
-                "abstract": row['abstract'],
-                "year": row['year'],
-                "dependents": [row['to_paper']]
-            }
+    for row in resultNode:
+        from_paper = row['from_paper']
+        to_paper = row['to_paper']
+        dictionary[from_paper] = {
+            "id": from_paper,
+            "title": row['from_title'],
+            "abstract": row['from_abstract'],
+            "year": row['from_year'],
+            "dependents": []
+        }
+        dictionary[to_paper] = {
+            "id": to_paper,
+            "title": row['to_title'],
+            "abstract": row['to_abstract'],
+            "year": row['to_year'],
+            "dependents": []
+        }
+
+    for row in resultLink:
+        from_paper = row['from_paper']
+        dictionary[from_paper]['dependents'].append(row['to_paper'])
 
     relatives = []
     for key in dictionary:
