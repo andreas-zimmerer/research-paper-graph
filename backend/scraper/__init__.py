7cb1f318bf7e9742e9145103e349e4b3f7d62802
Fiona Guerin <fiona.guerin@tum.de>
2020-01-09 15:42:53 +0100

    Scraper: The scraper stores for each paper the corresponding authors, writing relations, references.

--- backend/scraper/__init__.py
+++ backend/scraper/__init__.py
@@ -15,6 +15,22 @@ def scrape():
         # Put the given paper into the database.
         post_paper(paperId, paperTitle, paperAbstract, paperYear)
 
+        # Get all relevant information for the author: id, name
+        authors = paper['authors']
+        for author in authors:
+            authorId = author['authorId']
+            authorName = author['name']
+            # Put the given author and writing relation into the database.
+            post_author(authorId, authorName)
+            post_write(paperId, authorId)
+
+        # Get all references
+        citations = paper['citations']
+        for citation in citations:
+            citationId = citation['paperId']
+            post_reference(paperId, citationId)
+
+
 def get_all_papers():
     """Determine the ids of all relevant research papers."""
     authorIds = [143993045, 144122431]
@@ -46,6 +62,40 @@ def post_paper(paper_id, paper_title, paper_abstract, paper_year):
             'authors':['kemper', 'neumann']
             }
     r = requests.post(url='http://127.0.0.1:5000/paper/', json=data)
+    print(r.status_code)
+
+def post_paper(paper_id, paper_title, paper_abstract, paper_year):
+    """Post the given paper into the database."""
+    data = {'id':paper_id,
+            'title':paper_title,
+            'abstract':paper_abstract,
+            'year':paper_year
+            }
+    r = requests.post(url='http://127.0.0.1:5000/paper/', json=data)
+    print(r.status_code)
+
+def post_author(author_id, author_name):
+    """Post the given author into the database."""
+    data = {'id':author_id,
+            'name':author_name,
+            }
+    r = requests.post(url='http://127.0.0.1:5000/author/', json=data)
+    print(r.status_code)
+
+def post_write(paper, author):
+    """Post the given writing relation into the database."""
+    data = {'paper':paper,
+            'author':author,
+            }
+    r = requests.post(url='http://127.0.0.1:5000/write/', json=data)
+    print(r.status_code)
+
+def post_reference(source, sink):
+    """Post the given reference into the database."""
+    data = {'from_paper':source,
+            'to_paper':sink,
+            }
+    r = requests.post(url='http://127.0.0.1:5000/reference/', json=data)
     print(r.status_code)
 
 scrape()
