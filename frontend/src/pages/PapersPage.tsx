8cf3ef7ee71d9c416e55082743412f87a4da3fea
Andreas Zimmerer <andreas.zimmerer@tum.de>
2020-01-22 22:01:35 +0100

    fixed linting

--- frontend/src/pages/PapersPage.tsx
+++ frontend/src/pages/PapersPage.tsx
@@ -24,7 +24,10 @@ export default class PapersPage extends Component<IProps, IState> {
     return (
       <div className="page">
         <Sidebar onSelectedPaperChanged={this.handlePaperChanged} />
-        <PaperGraph papers={this.state.allPapers} selectedPaper={this.state.selectedPaper} onSelectedPaperChanged={this.handlePaperChanged} />
+        <PaperGraph papers={this.state.allPapers}
+                    selectedPaper={this.state.selectedPaper}
+                    onSelectedPaperChanged={this.handlePaperChanged}
+        />
       </div>
     );
   }
@@ -35,7 +38,7 @@ export default class PapersPage extends Component<IProps, IState> {
     if (selectedPaper === undefined) {
       return;
     }
-    
+
     // Fetch the family of papers that are connected to this paper
     fetch('http://localhost:5000/search?keyword=')
       .then((response) => response.json())
